60736c15ca9a06326c019bdfe2806d607e4b6dec
Andrey Ivanov <9808017@gmail.com>
2020-04-20 15:45:29 +0300

    cosmetic change

--- examples/nil_as_interface_argument_test.go
+++ examples/nil_as_interface_argument_test.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 )
 
-// A nil pointer passed to a function as an interface does not pass nil verification
+// An interface value that holds a nil concrete value is itself non-nil
 
 func Example_nil_as_interface_argument() {
 	var x *some // nil
+
+	fmt.Printf("1. Value: %v, type: %T\n", x, x)
 
 	if x == nil {
 		fmt.Println("1. Nil found")
@@ -15,15 +17,22 @@ func Example_nil_as_interface_argument() {
 		fmt.Println("1. Nil is not found")
 	}
 
+	fmt.Println()
+
 	bar(x)
 
 	// Output:
+	// 1. Value: <nil>, type: *examples.some
 	// 1. Nil found
+	//
+	// 2. Value: <nil>, type: *examples.some
 	// 2. Nil is not found
 }
 
-func bar(x explainer) {
-	if x == nil {
+func bar(y explainer) {
+	fmt.Printf("2. Value: %v, type: %T\n", y, y)
+
+	if y == nil {
 		fmt.Println("2. Nil found")
 	} else {
 		fmt.Println("2. Nil is not found")
